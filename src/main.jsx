2e1c5b433859fae42b8f33d55afa417e40ded4c5
MOB210005212 <hariprasad.yalliah@techmobius.com>
2025-12-03 10:32:46 +0530

    Add BrowserRouter basename fix for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import store from './redux/store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/90pickles">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
