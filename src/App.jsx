919bd433daa658367f465312925d40b7883d6b86
MOB210005212 <hariprasad.yalliah@techmobius.com>
2025-12-03 12:00:11 +0530

    updated app.jsx file removed browser import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
@@ -7,7 +7,6 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Login from './pages/Login';
-
 import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
@@ -15,13 +14,13 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<div className="p-20 text-center text-2xl font-serif">Our Story - Coming Soon</div>} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="product/:id" element={<ProductDetails />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<div className="p-20 text-center text-2xl font-serif">Our Story - Coming Soon</div>} />
+        <Route path="admin" element={<AdminDashboard />} />
       </Routes>
     </Layout>
   );
