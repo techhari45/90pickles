11f26fd906bcaa714b28501a0786c99efa438a6c
MOB210005212 <hariprasad.yalliah@techmobius.com>
2025-12-03 10:55:39 +0530

    updated the latest code

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom'   // ✅ REQUIRED IMPORT
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
